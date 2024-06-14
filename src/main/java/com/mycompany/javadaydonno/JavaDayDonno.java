9b1f263f8de8e02353823a94c41a657089e060fe
Foysal23 <sahadahmed86@gmail.com>
2024-06-14 19:36:44 +0600

    this is the first time commit from netbeans

---
+++ src/main/java/com/mycompany/javadaydonno/JavaDayDonno.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javadaydonno;
+
+/**
+ *
+ * @author Faysal
+ */
+public class JavaDayDonno {
+
+    static void student(){
+        int a,b,c;
+        
+        a=3;
+        b=4;
+        System.out.print("this is the new method or function! "+(c=a*b));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        student();
+    }
+}
